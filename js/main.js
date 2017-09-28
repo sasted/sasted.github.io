730b0f973598930d70bd09cdb9bca400fbb69181
sasted <32307001+sasted@users.noreply.github.com>
2017-09-28 14:11:09 -0700

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var bot = new cleverbot("d6KtnvVdOrpC0YOS", "LDlghx64ikzb2iFWdbhYn5Oe02rQnST3");
-bot.create(function (err, "VTmllFYBAaU71Zjq") {
-	function queryBot() {
-		var query = document.getElementById('query').value
-		bot.ask("Just a small town girl", function (err, response) {
-  document.getElementById("responseArea").innerHTML=response;
-});
-	}
-});
